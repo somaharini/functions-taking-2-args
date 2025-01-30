4a84fa0305db2ce53eec03671527f55bf8787731
somaharini <somaharini24@gmail.com>
2025-01-30 22:31:21 +0530

    Create Sumfunction.java

---
+++ Sumfunction.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Sumfunction {
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        int result = sum(num1, num2);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+        scanner.close();
+    }
+}
